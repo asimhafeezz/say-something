b30a96abfefb51464b4351ee948457f60af4ab0e
asimhyff <asimhaff.social@gmail.com>
2021-08-09 16:40:52 +0500

    updated fs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Say, Sayings, Footer } from './components';
-import './styles/index.scss';
+import { Say, Sayings, Footer } from "./components"
+import "./styles/index.scss"
 
 const App: React.FC = () => {
-  return(
-    <div>
-      <Say />
-      <Sayings />
-      <Footer />
-    </div>
-  )
+	return (
+		<div>
+			<Say />
+			<Sayings />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
